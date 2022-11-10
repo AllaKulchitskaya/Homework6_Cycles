20aa45af661c06ddf8b2cf55651c7ed3db6ab74b
AllaKulchitskaya <alla070@list.ru>
2022-11-10 16:23:16 +0300

    Дз 2, Задача 1

---
+++ src/Task1_2.java
@@ -0,0 +1,7 @@
+public class Task1_2 {
+    public static void main(String[] args) {
+        for (int i = 1904; i <= 2096; i = i+4) {
+            System.out.println(i + " - високосный год");
+        }
+    }
+}
